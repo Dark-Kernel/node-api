e0b0e65ff44f3bb734d7bc32372acc1d2b50e315
Dark-Kernel <Mr-SimpleEditor@protonmail.com>
2022-11-09 12:53:49 +0530

    renamed vars

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const PORT = process.env.PORT || 8080
-const axios = require('axios') // https url access (like curl)
+const axios = require('axios') // https link access (like clink)
 const cheerio = require('cheerio') // we scraper access classes
 const fs = require('fs')
-const express = require('express') // target url, to follow root
+const express = require('express') // target link, to follow root
 const app = express()
 const cors = require('cors') // headers
 app.use(cors())
@@ -22,38 +22,38 @@ async function amazon(product) {
 		"Accept-Encoding": "gzip, deflate, br",
 		"Connection": "keep-alive",
 	}
-	let url0 = `https://www.amazon.in/s?k=${product}&ref=nb_sb_noss_1`
-	const resp =  axios.get(url0, { headers })
+	let link0 = `https://www.amazon.in/s?k=${product}&ref=nb_sb_noss_1`
+	const resp =  axios.get(link0, { headers })
 		.then(response => {
 			const html = response.data
 			const $ = cheerio.load(html)
 			let site = "amazon"
 			let titles = []
 			let prices = []
-			let imgs = []
-			let urls = []
+			let hrefs = []
+			let links = []
 			let mrps = []
 			$('span.a-offscreen', html).each(function () { mrps.push($(this).text().replace('₹',''));  })
 			$('span.a-text-normal', html).each(function () { titles.push($(this).text().replace(/^(MORE\ RESULTS)/g,'').replace(/^(RESULTS)/g,''));  })
 			$('span.a-price-whole', html).each(function () { prices.push($(this).text().replace('₹',''));  })
-			$('img.s-image', html).each(function () { imgs.push($(this).attr('src')); })
-			$('a.s-no-outline', html).each(function () { urls.push($(this).attr('href').replace(/^(\/)/,'https://www.amazon.in/')) })
+			$('img.s-image', html).each(function () { hrefs.push($(this).attr('src')); })
+			$('a.s-no-outline', html).each(function () { links.push($(this).attr('href').replace(/^(\/)/,'https://www.amazon.in/')) })
 			titles = titles.filter(function(e){return e});	
-			prices.slice(5);titles.slice(5);imgs.slice(5);urls.slice(5);mrps.slice(5)
+			prices.slice(5);titles.slice(5);hrefs.slice(5);links.slice(5);mrps.slice(5)
 			for (var i = 0; i < len; i++) {
-				imgs.filter(item => !"https://m.media-amazon.com/images/I/11hfR5Cq9GL._SS200_.png".includes(item))
+				hrefs.filter(item => !"https://m.media-amazon.com/images/I/11hfR5Cq9GL._SS200_.png".includes(item))
 				let title = titles[i]
-				let url = urls[i]
-				let img = imgs[i]
+				let link = links[i]
+				let href = hrefs[i]
 				let price = prices[i]
 				let mrp = mrps[i]
 				art0.push({
 					site,
-					title,
+					link,
+					href,
 					price,
+					title,
 					mrp,
-					url,
-					img,
 				})
 			}
 			return art0;
@@ -66,15 +66,15 @@ async function flipkart(product){
 
 
 	var art1=[];
-	let url = `https://www.flipkart.com/search?q=${product}` // scraping url
-	const resp =  axios(url)
+	let link = `https://www.flipkart.com/search?q=${product}` // scraping link
+	const resp =  axios(link)
 		.then(response => {
 			const html = response.data
 			const $ = cheerio.load(html)
 
 	
-/*				// debugged- fixed url and title not found error.
-	    fs.writeFile("flipkart.html", html, (err) => {
+				// debugged- fixed link and title not found error.
+/*	    fs.writeFile("flipkart.html", html, (err) => {
 		      if (err)
 				console.log(err);
 		      else { console.log("done" )} 
@@ -83,17 +83,17 @@ async function flipkart(product){
 			let site = "flipkart"	
 			let titles = []
 			let prices = []
-			let imgs = []
-			let urls = []
+			let hrefs = []
+			let links = []
 			let mrps = []
 
 			$('div._27UcVY', html).each(function () { mrps.push($(this).text().replace('₹',''));  })
 			$('img._396cs4', html).each(function () { titles.push($(this).attr('alt'));  })
 			$('div._30jeq3', html).each(function () { prices.push($(this).text().replace('₹',''));  })
-			$('img._396cs4', html).each(function () { imgs.push($(this).attr('src')); })
-			$('a._1fQZEK', html).each(function () { urls.push($(this).attr('href').replace(/^(\/)/,'https://www.flipkart.com/').replace(/\?.*/,'')); })
-			if(!urls.length){
-				$('a._2rpwqI', html).each(function () { urls.push($(this).attr('href').replace(/^(\/)/,'https://www.flipkart.com/').replace(/\?.*/,'')); })
+			$('img._396cs4', html).each(function () { hrefs.push($(this).attr('src')); })
+			$('a._1fQZEK', html).each(function () { links.push($(this).attr('href').replace(/^(\/)/,'https://www.flipkart.com/').replace(/\?.*/,'')); })
+			if(!links.length){
+				$('a._2rpwqI', html).each(function () { links.push($(this).attr('href').replace(/^(\/)/,'https://www.flipkart.com/').replace(/\?.*/,'')); })
 			}
 			if(!mrps.length){
 				$('div._3I9_wc', html).each(function () { mrps.push($(this).text().replace('₹','')); })
@@ -102,18 +102,18 @@ async function flipkart(product){
 			for (let i = 1; i < len; i++) {
 
 				let title = titles[i]
-				let url = urls[i]
-				let img = imgs[i]
+				let link = links[i]
+				let href = hrefs[i]
 				let price = prices[i]
 				let mrp = mrps[i]
 
 				art1.push({
 					site,
-					title,
+					link,
+					href,
 					price,
+					title,
 					mrp,
-					url,
-					img,
 
 				})
 			}
@@ -128,25 +128,25 @@ async function reliance(product){
 
 
 	var art2=[]
-	let url = `https://www.reliancedigital.in/search?q=${product}` // scraping url
-	const resp =  axios(url)
+	let link = `https://www.reliancedigital.in/search?q=${product}` // scraping link
+	const resp =  axios(link)
 		.then(response => {
 			const html = response.data
 			const $ = cheerio.load(html)
 
-			/*
+			
 				// debugged: fixed the price not found && Product not found.
 	    fs.writeFile("reliance.html", html, (err) => {
 		      if (err)
 				console.log(err);
 		      else { console.log("done" )} 
 		}); 
-		*/
+		
 			let site = "reliance"
 			let titles = []
 			let prices = []
-			let imgs = []
-			let urls = []
+			let hrefs = []
+			let links = []
 			let mrps = []
 			let chk = ""
 
@@ -158,8 +158,8 @@ async function reliance(product){
 				$('span.StyledPriceBoxM__MRPText-sc-1l9ms6f-0', html).each(function () { mrps.push($(this).text().replace('₹',''));  })
 				$('p.sp__name', html).each(function () { titles.push($(this).text());  })
 				$('span.kCenjr', html).each(function () { prices.push($(this).text().replace('₹',''));  })
-				$('img.imgCenter', html).each(function () { imgs.push($(this).attr('data-srcset').replace(/^(\/)/,'https://www.reliancedigital.in/')); })
-				$('div.sp', html).each(function () { urls.push($(this).find('a').attr('href').replace(/^(\/)/,'https://www.reliancedigital.in/')); })
+				$('img.imgCenter', html).each(function () { hrefs.push($(this).attr('data-srcset').replace(/^(\/)/,'https://www.reliancedigital.in/')); })
+				$('div.sp', html).each(function () { links.push($(this).find('a').attr('href').replace(/^(\/)/,'https://www.reliancedigital.in/')); })
 
 				if(!prices.length){
 					$('span.llZwTv', html).each(function () { prices.push($(this).text());  })
@@ -168,19 +168,19 @@ async function reliance(product){
 				for (let i = 0; i < len; i++) {
 
 					let title = titles[i]
-					let url = urls[i]
-					let img = imgs[i]
+					let link = links[i]
+					let href = hrefs[i]
 					let price = prices[i]
 					let mrp = mrps[i]
 
 					if(!title==""){
 					art2.push({
 						site,
-						title,
+						link,
+						href,
 						price,
+						title,
 						mrp,
-						url,
-						img,
 
 					})
 					}
@@ -199,8 +199,8 @@ async function shopclues(product){
 
 
 	var art3=[]
-	let url3 = `https://bazaar.shopclues.com/search?q=${product}&sc_z=&z=0&count=15&user_id=&user_segment=default` // scraping url
-	const resp =  axios(url3)
+	let link3 = `https://bazaar.shopclues.com/search?q=${product}&sc_z=&z=0&count=15&user_id=&user_segment=default` // scraping link
+	const resp =  axios(link3)
 		.then(response => {
 			const html = response.data
 			const $ = cheerio.load(html)
@@ -217,8 +217,8 @@ async function shopclues(product){
 			let site = "shopclues"	
 			let titles = []
 			let prices = []
-			let imgs = []
-			let urls = []
+			let hrefs = []
+			let links = []
 			let mrps = []
 			let chk = ""
 			
@@ -229,31 +229,31 @@ async function shopclues(product){
 				//console.log("\n\n-----------NOT FOUND IN SHOPCLUES---------\n\n")
 			}else{
 				$('span.old_prices', html).each(function () { mrps.push($(this).text().replace('₹','').trim());  })
-				$('div.img_section', html).each(function () { titles.push($(this).find('img').attr('alt'));  })
+				$('div.img_section', html).each(function () { titles.push($(this).find('href').attr('alt'));  })
 				$('span.p_price', html).each(function () { prices.push($(this).text().replace('₹','').trim());  })
-				$('div.img_section', html).each(function () { imgs.push($(this).find('img').attr('data-img')); })
-				$('div.search_blocks', html).each(function () { urls.push($(this).find('a').attr('href').replace(/^(\/)/,'https:/')); })
+				$('div.img_section', html).each(function () { hrefs.push($(this).find('href').attr('data-href')); })
+				$('div.search_blocks', html).each(function () { links.push($(this).find('a').attr('href').replace(/^(\/)/,'https:/')); })
 
 				titles = titles.filter(function(e){return e});	
-				urls = urls.filter(function(e){return e});	
+				links = links.filter(function(e){return e});	
 
 				for (let i = 0; i < len; i++) {
 
-					imgs.filter(item => !"//cdn.shopclues.com//images/ui/plistlogo.png".includes(item))
+					hrefs.filter(item => !"//cdn.shopclues.com//images/ui/plistlogo.png".includes(item))
 					let title = titles[i]
-					let url = urls[i]
-					let img = imgs[i]
+					let link = links[i]
+					let href = hrefs[i]
 					let price = prices[i]
 					let mrp = mrps[i]
 					//console.log("\n\n",title," \n\n")
 					if(!title==""){
 					art3.push({
 						site,
-						title,
+						link,
+						href,
 						price,
+						title,
 						mrp,
-						url,
-						img,
 
 					})
 					}
